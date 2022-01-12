67429cfd0453b26ae1bc335fa191fdd6e1c1d119
unknown <matt.mcquain@gmail.com>
2022-01-11 20:20:58 -0800

    Hover download button in Pin.jsx works. Starting on other hover effects.

--- shareme_frontend/src/components/Pin.jsx
+++ shareme_frontend/src/components/Pin.jsx
@@ -8,9 +8,42 @@ import { BsFillArrowUpRightCircleFill } from 'react-icons/bs';
 import { urlFor, client } from '../client';
 
 const Pin = ({ pin: { postedBy, image, _id, destination }}) => {
+    const [postHovered, setPostHovered] = useState(false);
+    const [savingPost, setSavingPost] = useState(false);
+    const navigate = useNavigate();
+
   return (
-    <div>
-      <img className="rounded-lg w-full" alt='user-post' src={urlFor(image).width(250).url()} />
+    <div className="m-2">
+
+      {/* Hover Over Post Effect */}
+      <div
+        onMouseEnter={() => setPostHovered(true)}
+        onMouseLeave={() => setPostHovered(false)}
+        onClick={() => navigate(`/pin-detail/${_id}`)}
+        className="relative cursor-zoom-in w-auto hover:shadow-lg rounded-lg overflow-hidden transition-all duration-500 ease-in-out"
+      >
+        <img className="rounded-lg w-full" alt='user-post' src={urlFor(image).width(250).url()} />
+        {postHovered && (
+          <div
+            className="absolute top-0 w-full h-full flex flex-col justify-between p-1 pr-2 pt-2 pb-2 z-50"
+            style={{ height: '100%' }} //tailwindCSS giving issues with h-full so we need to set it manually here
+          >
+            <div className="flex items-center justify-between">
+              <div className='flex gap-2'>
+                <a
+                  href={`${image?.asset?.url}?dl=`} //allow download of image
+                  download
+                  onClick={(e) => e.stopPropagation()} //stops all hover effects from firing at once
+                  className="bg-white w-9 h-9 rounded-full flex items-center justify-center  text-dark text-xl opacity-75 hover:opacity-100 hover:shadow-md outline-none"
+                >
+                  <MdDownloadForOffline />
+                </a>
+              </div>
+            </div>
+          </div>
+        )}
+      
+      </div>      
     </div>
   )
 }
