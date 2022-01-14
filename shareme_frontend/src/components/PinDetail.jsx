3569fd7683d2a12686e78fa4169066b7444321b1
unknown <matt.mcquain@gmail.com>
2022-01-13 19:12:20 -0800

    basic PinDetail and its loading component working. Starting on details.

--- shareme_frontend/src/components/PinDetail.jsx
+++ shareme_frontend/src/components/PinDetail.jsx
@@ -1,6 +1,52 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { MdDownloadForOffline } from 'react-icons/md';
+import { Link, useParams } from 'react-router-dom';
+import { v4 as uuidv4 } from 'uuid';
 
-const PinDetail = () => {
+import { client, urlFor } from '../client';
+import MasonryLayout from './MasonryLayout';
+import { pinDetailMorePinQuery, pinDetailQuery } from '../utils/data';
+import Spinner from './Spinner';
+
+
+
+const PinDetail = ({ user }) => {
+  const [pins, setPins] = useState(null);
+  const [pinDetail, setPinDetail] = useState(null);
+  const [comment, setComment] = useState('');
+  const [addingComment, setAddingComment] = useState(false);
+  const { pinId } = useParams();
+
+
+
+  // Fetching Pin Details //
+  const fetchPinDetails = () => {
+    let query = pinDetailQuery(pinId);
+
+    if(query) {
+      client.fetch(query)
+        .then((data) => {
+          setPinDetail(data[0]); // first we get an individual pin and set it to pinDetail
+
+          if(data[0]) {
+            query = pinDetailMorePinQuery(data[0]); // then we get the pins that are related to the pin we just got
+
+            client.fetch(query)
+              .then((res) => setPins(res)); // get all related pins to that pin
+          }
+      });
+    }
+  }
+
+  useEffect(() => { 
+    fetchPinDetails();
+  }, [pinId]);
+
+  // Loading //
+  // remember to call conditional statements after useEffect. Just above return statement.
+  if(!pinDetail) return <Spinner message="Loading..." />;
+
+
   return (
     <div>
       PinDetail
